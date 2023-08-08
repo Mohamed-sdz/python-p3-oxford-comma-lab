23d068ea53d551e6ce5aafc6d92d0e03584e221e
saiderrahmani <didi.mousedidi@outlook.com>
2023-08-07 22:58:06 -0400

    update lib

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return "{} and {}".format(items[0], items[1])
+    else:
+        comma_items = ", ".join(items[:-1])
+        return "{}, and {}".format(comma_items, items[-1])
